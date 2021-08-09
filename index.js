78620fa8447bfa61674570c2ad35455a02f7d9d1
Wilfred <wilfredbayudan@gmail.com>
2021-08-08 17:42:00 -1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const input = document.getElementById("input");
+
 function addingEventListener() {
+  alert("CLICKED");
+  input.addEventListener("click", addingEventListener);
 }
+
+input.addEventListener("click", addingEventListener);
